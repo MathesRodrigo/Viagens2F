dcd017a022e7fa295e7c9ad89357ab197fea4b43
MathesRodrigo <matheusrodrigodesouza93@gmail.com>
2023-08-29 08:29:28 -0300

    bom dia

---
+++ src/Turista.java
@@ -0,0 +1,34 @@
+public class Turista {
+
+
+    private String nome;
+    private String cpf;
+
+    Turista() {}
+
+
+    Turista(String _nome) {
+        this.nome = _nome;
+    }
+
+
+    public String viajar(){
+        return "Viajou";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
